00cc7a7982c2ce05abafb00cbe4a060012b098ed
Sonia Wang <sonia@sonias-mbp.risd.edu>
2020-02-05 13:02:02 -0500

    first commit

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ function box41(){
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
     var whatisbox = document.querySelector(".whatisbox")
+    var text = document.querySelector(".codetext");
     box1.style.marginTop = "175px";
     box2.style.marginTop = "175px";
     box3.style.marginTop = "175px";
     whatisbox.style.visibility = "visible";
+    text.style.visibility = "visible";
     // box4.style.transform= "scale(1.5)";
     console.log("testing");
 }
@@ -18,49 +20,27 @@ function box42(){
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
     var whatisbox = document.querySelector(".whatisbox")
+    var text = document.querySelector(".codetext");
     box1.style.marginTop = "";
     box2.style.marginTop = "";
     box3.style.marginTop = "";
     box4.style.transform= "";
     whatisbox.style.visibility = "";
+    text.style.visibility = "";
     console.log("testing");
-}
-
-function plz1(){
-    var whatisbox = document.querySelector(".whatisbox")
-    var box4 = document.querySelector(".box4");
-    var box1 = document.querySelector(".box1");
-    var box2 = document.querySelector(".box2");
-    var box3 = document.querySelector(".box3");
-    whatisbox.style.visibility = "visible";
-    box4.style.animationPlayState = "paused";
-    box4.style.backgroundColor = "rgb(255, 255, 255)";
-    box1.style.marginTop = "175px";
-    box2.style.marginTop = "175px";
-    box3.style.marginTop = "175px";
-}
-
-function plz2(){
-    var whatisbox = document.querySelector(".whatisbox")
-    var box4 = document.querySelector(".box4");
-    var box1 = document.querySelector(".box1");
-    var box2 = document.querySelector(".box2");
-    var box3 = document.querySelector(".box3");
-    whatisbox.style.visibility = "";
-    box4.style.animationPlayState = "";
-    box4.style.backgroundColor = "";
-    box1.style.marginTop = "";
-    box2.style.marginTop = "";
-    box3.style.marginTop = "";
 }
 
 function box31(){
     var box1 = document.querySelector(".box1");
     var box2 = document.querySelector(".box2");
     var box4 = document.querySelector(".box4");
+    var coffee = document.querySelector(".coffee");
+    var teacup = document.querySelector(".teacup");
     box1.style.marginTop = "175px";
     box2.style.marginTop = "175px";
     box4.style.marginTop = "-50px";
+    coffee.style.visibility = "visible";
+    teacup.style.visibility = "visible";
     console.log("testing");
 }
 
@@ -68,9 +48,13 @@ function box32(){
     var box1 = document.querySelector(".box1");
     var box2 = document.querySelector(".box2");
     var box4 = document.querySelector(".box4");
+    var coffee = document.querySelector(".coffee");
+    var teacup = document.querySelector(".teacup");
     box1.style.marginTop = "";
     box2.style.marginTop = "";
     box4.style.marginTop = "";
+    coffee.style.visibility = "";
+    teacup.style.visibility = "";
     console.log("testing");
 }
 
@@ -79,9 +63,13 @@ function box21(){
     var box1 = document.querySelector(".box1");
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
+    var mount = document.querySelector(".mount");
+    var mountains = document.querySelector(".mountains");
     box1.style.marginTop = "175px";
     box3.style.marginTop = "-50px";
     box4.style.marginTop = "-50px";
+    mount.style.visibility = "visible";
+    mountains.style.visibility = "visible";
     console.log("testing");
 }
 
@@ -89,9 +77,13 @@ function box22(){
     var box1 = document.querySelector(".box1");
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
+    var mount = document.querySelector(".mount");
+    var mountains = document.querySelector(".mountains");
     box1.style.marginTop = "";
     box3.style.marginTop = "";
     box4.style.marginTop = "";
+    mount.style.visibility = "";
+    mountains.style.visibility = "";
     console.log("testing");
 }
 
@@ -100,9 +92,13 @@ function box11(){
     var box2 = document.querySelector(".box2");
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
+    var boob = document.querySelector(".boob");
+    var responsee = document.querySelector(".responsee");
     box2.style.marginTop = "-50px";
     box3.style.marginTop = "-50px";
     box4.style.marginTop = "-50px";
+    boob.style.visibility = "visible";
+    responsee.style.visibility = "visible";
     console.log("testing");
 }
 
@@ -110,8 +106,12 @@ function box12(){
     var box2 = document.querySelector(".box2");
     var box3 = document.querySelector(".box3");
     var box4 = document.querySelector(".box4");
+    var boob = document.querySelector(".boob");
+    var responsee = document.querySelector(".responsee");
     box2.style.marginTop = "";
     box3.style.marginTop = "";
     box4.style.marginTop = "";
+    boob.style.visibility = "";
+    responsee.style.visibility = "";
     console.log("testing");
 }
